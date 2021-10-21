3892963f41b149cd9982b6fd352a5ae95421d74c
ranthy <realthingshakes25@gmail.com>
2021-10-22 00:33:09 +0900

    First Commit. AtCoderBeginnersSelectionの実施

---
+++ AtCoderBeginnersSelection/README.md
@@ -0,0 +1,4 @@
+# AtCoder Beginners Selection
+
+## practiceA
+[AtCoderリンク](https://atcoder.jp/contests/abs/tasks/practice_1)
---
+++ AtCoderBeginnersSelection/practice_a.py
@@ -0,0 +1,9 @@
+def main():
+    a = input()
+    b, c = input().split(" ")
+    s = input()
+    print("{} {}".format(int(a)+int(b)+int(c), s))
+
+
+if __name__ == "__main__":
+    main()
