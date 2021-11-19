f8529e76c89a373723cb069af7ef19182e4db85f
ranthy <realthingshakes25@gmail.com>
2021-11-20 02:14:33 +0900

    ABC086C問題AC。

--- AtCoderBeginnersSelection/ABC086C.py
+++ AtCoderBeginnersSelection/ABC086C.py
@@ -1,11 +1,28 @@
 def main():
     N = int(input())
-    txy_dict = {}
+    ts, xs, ys = [0], [0], [0]
+    flg = False
     for _ in range(N):
         t, x, y = list(map(int, input().split()))
-        txy_dict[t] = (x, y)
+        ts.append(t)
+        xs.append(x)
+        ys.append(y)
 
-    print(txy_dict)
+    for i in range(N):
+        dt = ts[i+1] - ts[i]
+        dist = abs(xs[i+1] - xs[i]) + abs(ys[i+1] - ys[i])
+        if dist > dt:
+            flg = False
+        else:
+            if (dt + dist) % 2 == 0:
+                flg = True
+            else:
+                flg = False
+
+    if flg == True:
+        print("Yes")
+    else:
+        print("No")
 
 
 if __name__ == "__main__":
