dfbd2b285f3c9d103a83fbf520eba6d1acf250d8
ranthy <realthingshakes25@gmail.com>
2021-12-05 11:58:06 +0900

    ABC096C問題AC確認。

--- AtCoderBeginnersAdditional/ABC096C.py
+++ AtCoderBeginnersAdditional/ABC096C.py
@@ -10,24 +10,41 @@ def main():
         for w in range(1, W-1):
             if S_numpy[h, w] == "#":
                 if (S_numpy[h-1][w] == '.') and (S_numpy[h][w-1] == '.')\
-                        and (S_numpy[h][w+1] == '.') and (S_numpy[h+1][w] == '.'):
+                    and (S_numpy[h][w+1] == '.') \
+                        and (S_numpy[h+1][w] == '.'):
                     NO_flg = 1
 
-    for h in range(H):
-        if S_numpy[h, w] == "#" and h != (H-1):
-            if (S_numpy[h-1][w] == '.') and (S_numpy[h][w-1] == '.')\
-                    and (S_numpy[h][w+1] == '.') and (S_numpy[h+1][w] == '.'):
+        if S_numpy[h, 0] == "#":
+            if (S_numpy[h-1][0] == '.') and (S_numpy[h][1] == '.')\
+                    and (S_numpy[h+1][0] == '.'):
                 NO_flg = 1
-        else:
-            pass
+        if S_numpy[h, W-1] == "#":
+            if (S_numpy[h-1][W-1] == '.') and (S_numpy[h][W-2] == '.')\
+                    and (S_numpy[h+1][W-1] == '.'):
+                NO_flg = 1
 
-    for sn in S_numpy:
-        tmp = []
-        for s in sn:
-            if s == "#"
+    for w in range(1, W-1):
+        if S_numpy[0, w] == "#":
+            if (S_numpy[0][w-1] == '.')\
+                    and (S_numpy[0][w+1] == '.') and (S_numpy[1][w] == '.'):
+                NO_flg = 1
+        if S_numpy[H-1, w] == "#":
+            if (S_numpy[H-1][w-1] == '.')\
+                    and (S_numpy[H-1][w+1] == '.') and (S_numpy[H-2][w] == '.'):
+                NO_flg = 1
 
-    print(S_list)
-    print(S_numpy[:, 0])
+    if S_numpy[0, 0] == "#":
+        if (S_numpy[0][1] == '.') and (S_numpy[1][0] == '.'):
+            NO_flg = 1
+
+    if S_numpy[H-1, W-1] == "#":
+        if (S_numpy[H-2][W-1] == '.') and (S_numpy[H-1][W-2] == '.'):
+            NO_flg = 1
+
+    if NO_flg:
+        print("No")
+    else:
+        print("Yes")
 
 
 if __name__ == "__main__":
