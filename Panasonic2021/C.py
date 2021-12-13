0d40cc4d9f8fa9c3c38372ec8d5e2f327367967f
ranthy <realthingshakes25@gmail.com>
2021-12-13 19:12:12 +0900

    C問題のAC確認。

--- Panasonic2021/C.py
+++ Panasonic2021/C.py
@@ -1,10 +1,18 @@
-# TLE
+# 解説を見た上でAC確認
 def main():
-    _, Q = list(map(int, input().split()))
-    A_list = list(map(int, input().split()))
+    N, Q = list(map(int, input().split()))
+    A_list = sorted(map(int, input().split()))
     Q_list = [int(input()) for _ in range(Q)]
     for q in Q_list:
-        print(sorted(A_list+[q])[::-1].index(q))
+        right = N
+        left = -1
+        while right - left > 1:
+            mid = (right + left) // 2
+            if A_list[mid] >= q:
+                right = mid
+            else:
+                left = mid
+        print(N - right)
 
 
 if __name__ == "__main__":
