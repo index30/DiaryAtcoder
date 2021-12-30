3899e89005cbbaf968bdd0822d8d3fc8bae91cf0
ranthy <realthingshakes25@gmail.com>
2021-12-30 15:17:14 +0900

    Regular132の実施。

---
+++ Regular132/B.py
@@ -0,0 +1,22 @@
+def rec_proc(rp):
+    rp_list = rp[0]
+
+
+def main():
+    n = int(input())
+    p_list = list(map(int, input().split()))
+    sorted_p_list = sorted(p_list)
+    count = 0
+
+    while True:
+        if p_list == sorted_p_list:
+            break
+        # proc1
+        p_list =
+        # proc2
+
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
